8bff0fe39cb8300dce632d568385199b34fa385f
Evan Moore <etmoore@gmail.com>
2015-05-26 21:58:04 -0600

    defined userRoute

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   homeRoute(req, res);
+  userRoute(req, res);
 }).listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:3000/');
 
@@ -23,13 +24,20 @@ function homeRoute(req, res){
 }
 
 // Handle HTTP route for GET /:username
-function userRoute(){
+function userRoute(req, res){
   // if url == GET /....anything
+  var username = req.url.replace('/', '');
+  if (username.length > 0){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Header\n');
+    res.write(username + '\n');
+    res.end('Footer');
     // get JSON from Treehouse
       // on "end"
         // show profile
       // on 'error'
         // show error
+  }
 }
 
 // Function that handles the reading of files and merges in values
